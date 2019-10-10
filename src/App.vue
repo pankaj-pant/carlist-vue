22263dc424e2e8fde35d4fd196c10bb1504e7124
pankaj-pant <pankajpant89@gmail.com>
2019-10-10 11:01:38 +0300

    carlist

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Carlist/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Carlist from './components/Carlist.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Carlist
   }
 }
 </script>
---
+++ src/components/Carlist.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <h1>Car Table</h1>
+        <table>
+            <tbody>
+                <tr v-for="(car, index) in cars" v-bind:key="index">
+                    <td>{{car.brand}}</td>
+                    <td>{{car.model}}</td>
+                    <td>{{car.color}}</td>
+                    <td>{{car.fuel}}</td>
+                    <td>{{car.year}}</td>
+                    <td>{{car.price}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>    
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'Carlist',
+    data(){
+        return {
+            cars: []
+        }
+    },
+    mounted() {
+        axios.get('https://carstockrest.herokuapp.com/cars')
+        .then(response => (this.cars = response.data._embedded.cars))
+    }
+}
+</script>
+
+<style scoped>
+    table {
+        margin: auto;
+    }
+</style>
